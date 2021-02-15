0a1156b8b501022e33e9f02c34da1407aea6edbc
edvaldo <brungo1995@gmail.com>
2021-02-15 19:25:59 +0200

    added nav bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import { render } from 'react-dom'
+import rootReducer from "./Data/DataSources/slices"
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+import App from './App';
+
+const store = configureStore({ reducer: rootReducer });
+
+render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
@@ -15,7 +17,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/view/search/SearchView.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PrimarySearchAppBar from "../../Components/NavBar"
+
+export default function SearchView() {
+    return (
+        <PrimarySearchAppBar />
+    )
+}
